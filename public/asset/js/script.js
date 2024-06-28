e8df9d38e105bc63bf6b8f65a91c1d0e8513712f
Gimindu genuka <116905231+Gimindu@users.noreply.github.com>
2024-06-28 12:01:00 +0530

    Add files via upload

---
+++ public/asset/js/script.js
@@ -0,0 +1,42 @@
+/* ===============================navbar================================== */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+};
+
+
+/* =============================== for the form ================================== */
+
+let submit = document.getElementById("submit");
+submit.addEventListener('click',()=>{
+
+    alert(" click to redirct to your mail");
+    
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('number').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+    var body=`name: ${name} \n email: ${email}\n phone number: ${number}\n message: ${message} `;
+
+    const link = document.createElement('a');
+    link.href = `mailto:genuka56@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    document.body.append(link);
+    link.click();
+    document.body.remove(link);
+    window.location.href = "contact.html"
+})
+
+/* =============================== scroll reaveal ================================== */
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
